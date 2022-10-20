a6322abddd63be4075c5b66b546e429853aa7d6d
Daniel Grec <104867807+DanielGrec@users.noreply.github.com>
2022-10-20 20:43:44 +0300

    Initial Commit

---
+++ Unit Conversion/Unit_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionApp.swift
+//  Unit Conversion
+//
+//  Created by Grec Daniel on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
